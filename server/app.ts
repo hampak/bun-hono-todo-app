55bbde0cbeb5eeb1b055374c1db34e62812cfefc
Haemin Park <haemin.parque@gmail.com>
2024-08-01 20:48:58 +0900

    deploying to fly.io

--- server/app.ts
+++ server/app.ts
@@ -1,14 +1,16 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
 import { todosRoute } from "./routes/todos";
+import { serveStatic } from "hono/bun";
 
 const app = new Hono()
 
 app.use("*", logger())
 
-app.get("/", (c) => c.text("Hello bun"))
-
 app.route("/api/todos", todosRoute)
+
+app.get("*", serveStatic({ root: "./client/dist" }))
+app.get("*", serveStatic({ path: "./client/dist/index.html" }))
 
 
 export default app;
--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,7 @@
 import app from "./app"
 
 Bun.serve({
+
   fetch: app.fetch
 })
 
