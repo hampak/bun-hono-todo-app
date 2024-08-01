4288afdb98b1afed6f8a4929048b8ca6de4d4c6a
Haemin Park <haemin.parque@gmail.com>
2024-08-01 15:38:14 +0900

    added tailwindcss and shadcn

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react"
+
+interface Todo {
+  id: number;
+  title: string;
+  description: string
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [todos, setTodos] = useState<Todo[]>([])
+
+  useEffect(() => {
+    async function fetchTodos() {
+      const res = await fetch("/api/todos")
+      const data = await res.json()
+      setTodos(data.todos)
+    }
+
+    fetchTodos()
+  }, [])
+
+  console.log(todos)
+
+  if (!todos.length) {
+    return null
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      {todos[0].title}
+    </div>
   )
 }
 
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  // forward requests to localhost:3000 
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true
+      }
+    }
+  }
 })
