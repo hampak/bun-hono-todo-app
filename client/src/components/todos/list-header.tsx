6037984283ba1a3bd3733134aba469414bca56bd
Haemin Park <haemin.parque@gmail.com>
2024-08-04 20:46:25 +0900

    [IP] - working on the list header component + added separator

--- client/src/components/todos/list-header.tsx
+++ client/src/components/todos/list-header.tsx
@@ -1,11 +1,92 @@
+import { ElementRef, useEffect, useRef, useState } from "react"
+import { Input } from "../ui/input"
+import { Button } from "../ui/button"
 
 interface ListHeaderProps {
   title: string
 }
 
 export const ListHeader = ({ title }: ListHeaderProps) => {
+
+  const [isEditing, setIsEditing] = useState(false)
+  const inputRef = useRef<ElementRef<"input">>(null)
+  const formRef = useRef<HTMLFormElement>(null)
+  const buttonRef = useRef<HTMLButtonElement>(null);
+
+
+  const enableEditing = () => {
+    setIsEditing(true)
+    setTimeout(() => {
+      inputRef.current?.focus();
+      inputRef.current?.select();
+    })
+  }
+
+  const disableEditing = () => {
+    setIsEditing(false)
+  }
+
+  const handleClickOutside = (e: MouseEvent) => {
+    if (
+      formRef.current &&
+      !formRef.current.contains(e.target as Node) &&
+      buttonRef.current &&
+      !buttonRef.current.contains(e.target as Node)
+    ) {
+      disableEditing()
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside)
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  })
+
   return (
-    <>
-    </>
+    <div className="pt-2 px-2 text-sm font-semibold flex justify-between items-center gap-x-2 mb-4">
+      <div className="flex-1">
+        {
+          isEditing ? (
+            <div className="flex flex-col space-y-2">
+              <form
+                className=""
+                ref={formRef}
+              >
+                <input
+                  hidden
+                  id="id"
+                  name="id"
+                />
+                <Input
+                  defaultValue={title}
+                  placeholder="change the name"
+                  ref={inputRef}
+                />
+              </form>
+              <Button
+                ref={buttonRef}
+                className="w-full"
+                size="xs"
+                type="button"
+                onClick={(e) => {
+                  e.stopPropagation()
+                }}
+              >
+                Save
+              </Button>
+            </div>
+          ) : (
+            <p
+              className="w-full cursor-pointer"
+              onClick={enableEditing}
+            >
+              {title}
+            </p>
+          )
+        }
+      </div>
+    </div>
   )
 }
--- client/src/components/todos/list-item.tsx
+++ client/src/components/todos/list-item.tsx
@@ -1,5 +1,6 @@
 import { Lists } from "@server/db/schema/schema"
 import { ListHeader } from "./list-header"
+import { Separator } from "../ui/separator"
 
 interface ListItemProps {
   list: Lists
@@ -8,9 +9,10 @@ interface ListItemProps {
 export const ListItem = ({ list }: ListItemProps) => {
 
   return (
-    <li className="bg-green-300 w-[272px] select-none list-none">
-      <div className="w-full rounded-md g-[#f1f2f4] shadow-md pb-2">
+    <li className="bg-green-300s w-[272px] select-none list-none">
+      <div className="w-full rounded-md g-[#f1f2f4] shadow-md p-2">
         <ListHeader title={list.title} />
+        <Separator />
         <div className="space-y-1 px-1">
         </div>
       </div>
